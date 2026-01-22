79323e5cc3bfc20d9afc2de21da6ed4e10fbe2c2
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-01-22 22:03:31 +0530

    Made a separate ListItem Component to store the items

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,17 +1,7 @@
 import * as React from "react";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import CommentIcon from "@mui/icons-material/Comment";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import DeleteIcon from "@mui/icons-material/Delete";
 
+import TodoItem from "./TodoItem";
 import { useState } from "react";
 const initialTodos = [
   { id: 1, text: "Walk the dog", completed: false },
@@ -22,40 +12,9 @@ export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      {todos.map((todo) => {
-        const labelId = `checkbox-list-label-${todo.id}`;
-
-        return (
-          <ListItem
-            key={todo.id}
-            secondaryAction={
-              <IconButton edge="end" aria-label="comments">
-                <Box sx={{ flexGrow: 1 }}>
-                  <Grid container sx={{ color: "text.primary" }}>
-                    <Grid size={8}>
-                      <DeleteIcon />
-                    </Grid>
-                  </Grid>
-                </Box>
-              </IconButton>
-            }
-            disablePadding
-          >
-            <ListItemButton role={undefined} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={todo.completed}
-                  tabIndex={-1}
-                  disableRipple
-                  // inputProps={{ 'aria-labelledby': labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={todo.text} />
-            </ListItemButton>
-          </ListItem>
-        );
-      })}
+      {todos.map((todo) => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
     </List>
   );
 }
